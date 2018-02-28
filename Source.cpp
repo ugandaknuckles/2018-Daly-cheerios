24934b57debbd7cdf97f5dca00d53beb63d8e046
ugandaknuckles <35495803+ugandaknuckles@users.noreply.github.com>
2018-02-28 08:34:52 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,23 @@
 using namespace std;
 
 int main() {
+
 	srand(time(NULL));
 	al_init();
 	al_init_primitives_addon();
 	ALLEGRO_DISPLAY *display = al_create_display(800, 800);
 
-	while (1) {
-		al_draw_filled_circle(rand()%800, rand()%800, rand()%200, al_map_rgb(rand()%256, 0,rand()%256));
-		al_rest(.02);
-		al_flip_display();
-	}
+	cout << "modern art" << endl;
+	
+	al_draw_filled_triangle(400, 600, 600, 400, 600, 100, al_map_rgb(400, 0, 200));
+	al_draw_filled_circle(400, 300, 60, al_map_rgb(255, 255, 255));
+	al_draw_filled_circle(700, 300, 60, al_map_rgb(255, 255, 255));
+	al_draw_line(750, 100, 400, 100, al_map_rgb(255, 255, 255), 5);
+	al_draw_filled_ellipse(460, 700, 200, 100, al_map_rgb(600, 40, 40));
 
+	al_flip_display();
+	al_rest(10);
 	al_destroy_display(display);
 
+	system("pause");
 }
